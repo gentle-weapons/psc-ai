70caaf522b24dc170e1fc14f877d5c22cce20618
Stockton Mounteer <42601566+stockmounteer@users.noreply.github.com>
2026-03-24 23:15:37 -0700

    add review page at /review

---
+++ apps/platform/app/review/page.js
@@ -0,0 +1,9 @@
+export default function Consumer() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p className="text-2xl tracking-widest uppercase font-light">
+        PSC AI: Review Form
+      </p>
+    </div>
+  );
+}
