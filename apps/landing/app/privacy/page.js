fe5b2433dec4d71d3dba584b48eaf74875a80cf3
Stockton Mounteer <42601566+stockmounteer@users.noreply.github.com>
2026-03-26 14:33:58 -0700

    add legal page files and resolved /landing tailwindcss version conflicts

---
+++ apps/landing/app/privacy/page.js
@@ -0,0 +1,9 @@
+export default function Privacy() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p className="text-2xl tracking-widest uppercase font-light">
+        Privacy Policy
+      </p>
+    </div>
+  );
+}
---
+++ apps/landing/app/terms/page.js
@@ -0,0 +1,9 @@
+export default function Terms() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p className="text-2xl tracking-widest uppercase font-light">
+        Terms of Service
+      </p>
+    </div>
+  );
+}
